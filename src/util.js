1619135d763cacba30505bb115cd6fa9ffee741a
Hanxmao <hanxmao@hotmail.com>
2024-11-20 03:33:06 -0800

    add apiCall util

--- src/util.js
+++ src/util.js
@@ -3,3 +3,24 @@ export const getCookie = (name) => {
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts.pop().split(";").shift();
   };
+
+
+export  const apiCall = async (endpoint, options = {}) => {
+    try {
+      const response = await fetch(endpoint, options);
+      const data = await response.json();
+
+      const apiCalls = data.apiCalls;
+  
+      localStorage.setItem("apiCalls", apiCalls);
+  
+      if (apiCalls >= 20) {
+        alert("You have hit the 20 API call limit!");
+      }
+  
+      return data; 
+    } catch (error) {
+      console.error("API call failed:", error);
+    }
+  };
+  
