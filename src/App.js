f05988e5024042af7fa2b0e1a6ed1cdc8479a627
Hanxmao <hanxmao@hotmail.com>
2024-11-20 03:33:20 -0800

    add api limit warning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
@@ -11,9 +11,27 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import ProtectedRoute from "./components/protectedRoute";
 
+const API_LIMIT = 20;
+
 const App = () => {
+  const [apiCallLimitReached, setApiCallLimitReached] = useState(false);
+
+  useEffect(() => {
+    const apiCalls = parseInt(localStorage.getItem("apiCalls") || "0", 10);
+
+    if (apiCalls >= API_LIMIT) {
+      setApiCallLimitReached(true);
+    }
+  }, []);
+
   return (
     <Router>
+      {apiCallLimitReached && (
+        <div style={{ background: "red", color: "white", padding: "10px", textAlign: "center" }}>
+          You have hit the {API_LIMIT} API call limit!
+        </div>
+      )}
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
